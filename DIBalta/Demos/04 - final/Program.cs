a57fd42ee9acbedc4a200584be615730d1ff8886
fulldevfritz <fritzlumen@hotmail.com>
2024-11-03 14:48:43 -0300

    add estudos DI

--- DIBalta/Demos/04 - final/Program.cs
+++ DIBalta/Demos/04 - final/Program.cs
@@ -3,11 +3,20 @@ using Microsoft.Extensions.DependencyInjection.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<IService, PrimaryService>();
+//builder.Services.TryAddTransient<IService, PrimaryService>();
+builder.Services.AddTransient<IService, PrimaryService>();
 // builder.Services.AddTransient<IService, PrimaryService>();
-// builder.Services.AddTransient<IService, PrimaryService>();
+//builder.Services.TryAddTransient<IService, SecondaryService>();
 // builder.Services.AddTransient<IService, SecondaryService>();
-// builder.Services.AddTransient<IService, SecondaryService>();
-// builder.Services.AddTransient<IService, TertiaryService>();
+builder.Services.AddTransient<IService, TertiaryService>();
+
+// builder.Services.TryAddTransient<IService, PrimaryService>();
+// builder.Services.TryAddTransient<IService, PrimaryService>();
+// builder.Services.TryAddTransient<IService, PrimaryService>();
+
+//builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>());
+//builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>()); // Permite
+//builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, SecondaryService>()); // Não permite
 
 // var descriptor = new ServiceDescriptor(
 //     typeof(IService),
@@ -15,16 +24,11 @@ builder.Services.AddTransient<IService, PrimaryService>();
 //     ServiceLifetime.Transient);
 // builder.Services.Add(descriptor);
 
-// builder.Services.TryAddTransient<IService, PrimaryService>();
-// builder.Services.TryAddTransient<IService, PrimaryService>();
-// builder.Services.TryAddTransient<IService, PrimaryService>();
-
-// builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>());
-// builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, PrimaryService>()); // Permite
-// builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IService, SecondaryService>()); // Não permite
-
 var app = builder.Build();
 
+// app.MapGet("/", (IService service) => {
+//     return Results.Ok(service.GetType().Name);
+// });
 // app.MapGet("/", (IService primaryService) => Results.Ok(primaryService.GetType().Name));
 app.MapGet("/", (IEnumerable<IService> services) => Results.Ok(services.Select(x=>x.GetType().Name)));
 
